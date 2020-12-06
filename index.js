22c9a23fb1bcd93aebca9741fb163fa708995356
jsiegenthaler <jbsiegenthaler@gmail.com>
2020-12-06 23:28:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -336,19 +336,19 @@ tvAccessory.prototype = {
 			// only for be-nl and be-fr users, as the session logon using openid is different
 		this.log('getSessionBE');
 
-		let fetchuri = countryBaseUrlArray[this.config.country] + '/authorization';
+		let authorizationUri = countryBaseUrlArray[this.config.country] + '/authorization';
 		// https://web-api-prod-obo.horizon.tv/oesp/v3/BE/nld/web/authorization
 		this.log('Using fetch',fetchuri);
-		fetch("https://web-api-prod-obo.horizon.tv/oesp/v3/BE/nld/web/authorization", {
+		fetch(authorizationUri, {
 			"headers": {
 			  "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-			  "accept-language": "en-GB,en;q=0.9,de;q=0.8,en-US;q=0.7,fr;q=0.6",
+		//	  "accept-language": "en-GB,en;q=0.9,de;q=0.8,en-US;q=0.7,fr;q=0.6",
 			  "cache-control": "max-age=0",
-			  "sec-fetch-dest": "document",
-			  "sec-fetch-mode": "navigate",
-			  "sec-fetch-site": "cross-site",
-			  "sec-fetch-user": "?1",
-			  "upgrade-insecure-requests": "1"
+		//	  "sec-fetch-dest": "document",
+		//	  "sec-fetch-mode": "navigate",
+		///	  "sec-fetch-site": "cross-site",
+		//	  "sec-fetch-user": "?1",
+		//	  "upgrade-insecure-requests": "1"
 			},
 			"referrerPolicy": "no-referrer",
 			"body": null,
