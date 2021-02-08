c06385c32f31b783f8daa2de8447942b15e6aaad
jsiegenthaler <jbsiegenthaler@gmail.com>
2021-02-08 19:06:21 +0100

    Update index.js

--- index.js
+++ index.js
@@ -457,7 +457,21 @@ tvAccessory.prototype = {
 							headers: {
 								'Cache-Control': 'max-age=0',
 								'Content-Type': 'application/x-www-form-urlencoded',
-								//'Connection': 'keep-alive',
+
+								'Upgrade-Insecure-Requests' : 1,
+								'Accept' : 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+								'Accept-Encoding' : 'gzip, deflate, br',
+								'Accept-Language' : 'nl-NL,nl;q=0.9',
+								'Connection' : 'keep-alive',
+								'Host' : 'login.prd.telenet.be',
+								'sec-ch-ua' : '"Chromium";v="88", "Google Chrome";v="88", ";Not A Brand";v="99"',
+								'sec-ch-ua-mobile' : '?0',
+								'Sec-Fetch-Dest' : 'document',
+								'Sec-Fetch-Mode' : 'navigate',
+								'Sec-Fetch-Site' : 'none',
+								'Sec-Fetch-User' : '?1',
+								'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.104 Safari/537.36',
+
 								//'Cookie':'Cookie1=Value1',
 								},
 							data: qs.stringify({
